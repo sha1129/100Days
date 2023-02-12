57209e582047b3d631693f8105933f1f4044da08
shan1129 <sns1113@protonmail.com>
2023-02-11 19:52:52 -0500

    FIRST_VALUE use

--- SQL/L33TC0DE/EASY/(511) - Game Play Analysis I.sql
+++ SQL/L33TC0DE/EASY/(511) - Game Play Analysis I.sql
@@ -1,8 +1,17 @@
 -- Write an SQL query to report the first login date for each player.
 
-SELECT distinct player_id , MIN(event_date) AS first_login 
+SELECT DISTINCT player_id , MIN(event_date) AS first_login 
 FROM Activity
 GROUP BY player_id
 
 
 -- WAS bit trickey as I forgot all the tricks 
+
+
+-- NEW knowledge
+-- first_value
+
+SELECT DISTINCT player_id,
+FIRST_VALUE (event_date) 
+    OVER( PARTITION BY player_id ORDER BY event_date ASC) AS first_login
+FROM Activity
