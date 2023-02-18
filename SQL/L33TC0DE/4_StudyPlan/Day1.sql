98ddeaba0a7872926345fe86266af17229e7c822
shan1129 <sns1113@protonmail.com>
2023-02-18 17:40:47 -0500

    Minor Changes on Notes

--- SQL/L33TC0DE/4_StudyPlan/Day1.sql
+++ SQL/L33TC0DE/4_StudyPlan/Day1.sql
@@ -1,6 +1,8 @@
 --      EASY    --
+----------------------------------------------------------------
 -- 595. Big Countries 
 ----------------------------------------------------------------
+
 --A country is big if:
 
     -- it has an area of at least three million (i.e., 3000000 km2), or
@@ -11,19 +13,21 @@
 SELECT  name, population, area AS 'area'
 FROM World
 WHERE area >= 3000000 OR population >=  25000000
-
+----------------------------------------------------------------
 -- 1757. Recyclable and Low Fat Products
 ----------------------------------------------------------------
+
 -- Write an SQL query to find the ids of products that are both low fat and recyclable.
 
 SELECT product_id  
 FROM Products
 WHERE low_fats = 'Y' AND recyclable = 'Y'
-
 -- Have to use AND here
 
+----------------------------------------------------------------
 -- 584. Find Customer Referee
 ----------------------------------------------------------------
+
 -- Write an SQL query to report the names of the customer that are not 
 -- referred by the customer with id = 2.
 
@@ -32,7 +36,7 @@ FROM Customer
 WHERE referee_id NOT LIKE '2' OR referee_id is NUll
 
 -- NEED TO SELECT THE NULLs As null is not a number
-
+----------------------------------------------------------------
 -- 183. Customers Who Never Order
 ----------------------------------------------------------------
 
