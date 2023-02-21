b530eadb752ad2a98b94088fe271a6902a009ade
shan1129 <sns1113@protonmail.com>
2023-02-20 20:01:05 -0500

    Missing code added

--- SQL/L33TC0DE/4_StudyPlan/Day5.sql
+++ SQL/L33TC0DE/4_StudyPlan/Day5.sql
@@ -35,4 +35,10 @@ ORDER BY customer_id ASC
 -- 1148. Article Views I
 ----------------------------------------------------------------
 
+--Write an SQL query to find all the authors that viewed at least one of their own articles.
+--Return the result table sorted by id in ascending order.
 
+SELECT DISTINCT author_id AS id
+FROM Views  
+WHERE author_id  = viewer_id   
+ORDER BY id ASC 
