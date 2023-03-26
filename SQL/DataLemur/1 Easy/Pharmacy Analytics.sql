59b1738b5258739ed86b897d6516a8a0795cc81b
shan1129 <sns1113@protonmail.com>
2023-03-26 19:21:29 -0400

    Ok

---
+++ SQL/DataLemur/1 Easy/Pharmacy Analytics.sql
@@ -0,0 +1,13 @@
+-- part 1
+-- CVS Health SQL Interview Question
+
+-- CVS Health is trying to better understand its pharmacy sales, and how well different products are selling. 
+    -- Each drug can only be produced by one manufacturer.
+
+-- Write a query to find the top 3 most profitable drugs sold, and how much profit they made.
+   -- Assume that there are no ties in the profits. Display the result from the highest to the lowest total profit.
+
+SELECT DISTINCT drug, total_sales-cogs AS total_profit
+FROM pharmacy_sales
+ORDER BY total_profit DESC
+limit 3;
